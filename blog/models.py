057ae4b51ab52bc586b2b18bfce8e5d997a35e21
makar <ma.karami81@gmail.com>
2024-01-11 21:33:41 +0330

    add class of two field create and publish and other class inherit from it

--- blog/models.py
+++ blog/models.py
@@ -3,29 +3,38 @@ from django.conf import settings
 from django.utils import timezone
 # Create your models here.
 
-
-class Tag(models.Model):
-    title = models.CharField(max_length=50,unique = True)
+class publish_create_field(models.Model):
     created_date=models.DateTimeField(default=timezone.now)
     publish_date=models.DateTimeField(blank=True,null=True)
-    color_label = models.CharField(max_length=50,blank=True)
-
     def publish(self):
         self.publish_date = timezone.now()
         self.save()
+    class Meta:
+        abstract = True
+
+
+class Tag(publish_create_field):
+    title = models.CharField(max_length=50,unique = True)
+    #created_date=models.DateTimeField(default=timezone.now)
+    #publish_date=models.DateTimeField(blank=True,null=True)
+    color_label = models.CharField(max_length=50,blank=True)
+    
+    #def publish(self):
+        #self.publish_date = timezone.now()
+        #self.save()                             
     def __str__(self):
         return self.title
     
-class Post(models.Model):
+class Post(publish_create_field):
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     text = models.TextField()
-    created_date=models.DateTimeField(default=timezone.now)
-    publish_date=models.DateTimeField(blank=True,null=True)
+    #created_date=models.DateTimeField(default=timezone.now)
+    #publish_date=models.DateTimeField(blank=True,null=True)
     tags = models.ManyToManyField(Tag)
-    def publish(self):
-        self.publish_date=timezone.now()
-        self.save()
+    #def publish(self):
+        #self.publish_date=timezone.now()
+        #self.save()
     def __str__(self):
         return self.title
     def approved_comment(self):
